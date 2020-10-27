318182389a8430b83523fda9e3a65ffb559db3e3
Sergey <serge.dmitriev@gmail.com>
2020-10-27 16:02:31 +0700

    fix

---
+++ index.js
@@ -0,0 +1,32 @@
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const venom = require('venom-bot');
+
+venom
+  .create(
+    'sessionName',
+    console.log,
+    console.log,
+    {
+        headless:false,
+    }    
+  )
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome Venom 🕷')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
